0827054909f88f4c704fea99cc066b711d1d79ed
SusiSusi <suvi.sipila@helsinki.fi>
2016-01-20 22:09:20 +0200

    seiska tehty

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
 # kirjoita koodi tänne
+def keskiarvo(taulukko)
+	pituus = taulukko.length.to_f
+	tulos = taulukko.inject(0){ |tulos, x | tulos + x}
+	return tulos/pituus
+end
+
+taulukko = [1,2]
+tulos = keskiarvo(taulukko)
+puts tulos
