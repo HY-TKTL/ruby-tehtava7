ef0894abcc4239c89c160cb15a8ed0ae799359bb
Tontsu <Tontsu@Tontsus-MacBook-Air.local>
2016-01-21 13:04:25 +0200

    doned

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,9 @@
 # kirjoita koodi tänne
+def keskiarvo(taulukko)
+  summa = taulukko.inject(0) {|tulos, x| tulos + x}
+  summa/(taulukko.length * 1.0)
+end
+
+taulukko = [1, 2, 4]
+
+keskiarvo(taulukko)
